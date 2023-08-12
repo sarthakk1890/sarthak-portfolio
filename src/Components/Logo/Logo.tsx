829107f80a552ba6d3d6a4e423b64a40eee8a6fa
Sarthak Singh <144singhsarthak@gmail.com>
2023-08-12 16:10:35 +0530

    Removed GSAP

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -1,50 +1,22 @@
-import React, { useEffect, useRef } from 'react';
-import gsap from 'gsap';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import LogoS from '../../assets/images/logo-s.png';
+import React, { useState, useEffect, useRef } from 'react';
 import './Logo.css';
 
-const Logo: React.FC  = () => {
-  const bgRef = useRef(null);
-  const outlineLogoRef = useRef(null);
-  const solidLogoRef = useRef(null);
+const Logo: React.FC = () => {
+  const [isFading, setIsFading] = useState(false);
+  const bgRef = useRef<HTMLDivElement>(null);
+  const outlineLogoRef = useRef<SVGPathElement>(null);
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+    const logoContainer = bgRef.current;
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    )
-  }, [])
+    if (logoContainer) {
+      logoContainer.style.opacity = '1';
+      setIsFading(true);
+    }
+  }, []);
 
   return (
-    <div className="logo-container" ref={bgRef}>
-      <img
-        className="solid-logo"
-        ref={solidLogoRef}
-        src={LogoS}
-        alt="JavaScript,  Developer"
-      />
-
+    <div className={`logo-container ${isFading ? 'fade-in' : ''}`} ref={bgRef}>
       <svg
         width="559pt"
         height="897pt"
@@ -52,6 +24,7 @@ const Logo: React.FC  = () => {
         viewBox="0 0 559 897"
         xmlns="http://www.w3.org/2000/svg"
       >
+
         <g
           className="svg-container"
           transform="translate(0 457) scale(.1 -.1)"
